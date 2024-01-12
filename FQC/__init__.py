1e3faf8e488eb468d6777f3ba99fd999b524a183
Sgrneane <sgrneaneprof@gmail.com>
2024-01-12 16:24:22 +0545

    __init.py file

--- FQC/__init__.py
+++ FQC/__init__.py
@@ -1,6 +1,6 @@
-import pymysql
-from .celery import app as celery_app
+# import pymysql
+# from .celery import app as celery_app
 
-__all__ = ("celery_app",)
+# __all__ = ("celery_app",)
 
-pymysql.install_as_MySQLdb()
+# pymysql.install_as_MySQLdb()
